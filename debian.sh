de950fd7f018a4cb7b894ba0709a6a05f595bfe8
XDragon950 <101992388+XDragon950@users.noreply.github.com>
2024-08-14 20:07:29 -0400

    Update debian.sh

--- debian.sh
+++ debian.sh
@@ -1,5 +1,5 @@
 apt install sudo
 sudo usermod -aG sudo user
-apt install lightdm neofetch terminator i3 chromium zsh
+apt install lightdm neofetch terminator i3 chromium zsh git curl
 chsh user /bin/zsh
 reboot
