9c8322dd9c7985bb016456f9d3293bcebcb1e3fc
XDragon950 <101992388+XDragon950@users.noreply.github.com>
2024-08-15 20:30:55 -0400

    Update teamviewer.sh

--- teamviewer.sh
+++ teamviewer.sh
@@ -1,2 +1,3 @@
 wget https://download.teamviewer.com/download/linux/teamviewer-host_amd64.deb?utm_source=bing&utm_medium=cpc&utm_campaign=us%7Cb%7Cpr%7C22%7Caug%7Ctv-core-brand-only-sn%7Cnew%7Ct0%7C0&utm_content=Exact&utm_term=teamviewer
+mv teamviewer-host_amd64.deb?utm_source=bing team-viewer-host_15.55.3_amd64.deb
 sudo dpkg -i team-viewer-host_15.55.3_amd64.deb
