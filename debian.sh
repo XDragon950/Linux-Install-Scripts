e24bc08126add67e30bbb9a75283e965bb0cc555
XDragon950 <101992388+XDragon950@users.noreply.github.com>
2024-12-29 20:41:03 -0500

    Update debian.sh

--- debian.sh
+++ debian.sh
@@ -1,6 +1,6 @@
 # apt install sudo
 # sudo usermod -aG sudo user
-apt install neofetch terminator chromium git xinit curl htop -y
+apt install neofetch terminator chromium git xinit curl htop i3 thunar -y
 wget https://download.teamviewer.com/download/linux/teamviewer-host_amd64.deb
 sudo chmod +rwx team-viewer-host_15.55.3_amd64.deb
 ./team-viewer-host_15.55.3_amd64.deb
