745656da60397a4c2c3d85c13c8985cd31f96e48
XDragon950 <101992388+XDragon950@users.noreply.github.com>
2026-01-03 16:44:11 -0500

    Change default shell to zsh and reboot
    Added command to change the default shell to zsh and reboot the system.

--- debian.sh
+++ debian.sh
@@ -4,6 +4,7 @@
 wget https://cdn.fastly.steamstatic.com/client/installer/steam.deb
 sudo dpkg -i steam.deb
 sudo apt install fastfetch chromium xinit curl htop i3 thunar zsh -y
+chsh -s /bin/zsh
 curl -fsSL https://download.opensuse.org/repositories/home:clayrisser:sid/Debian_Unstable/Release.key | gpg --dearmor | sudo tee /etc/apt/keyrings/home_clayrisser_sid.gpg > /dev/null
 ARCH="$(dpkg --print-architecture)"
 sudo tee /etc/apt/sources.list.d/home:clayrisser:sid.sources > /dev/null <<EOF
@@ -17,3 +18,4 @@ sudo apt update
 sudo apt install ghostty -y
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 sed -i 's/robbyrussel/bira/g' ~/file.txt
+sudo reboot
