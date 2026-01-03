b93ae588c7a834ce8c8f6d3f2b360766a1d53048
XDragon950 <101992388+XDragon950@users.noreply.github.com>
2026-01-03 14:19:07 -0500

    Modify debian.sh for package installations and repos
    Updated package installations and added repository sources for Ghostty and Steam.

--- debian.sh
+++ debian.sh
@@ -1,6 +1,18 @@
+# the first two are required if you install with a root password and another user account
 # apt install sudo
-# sudo usermod -aG sudo user
-apt install neofetch terminator chromium git xinit curl htop i3 thunar -y
-wget https://download.teamviewer.com/download/linux/teamviewer-host_amd64.deb
-sudo chmod o+x team-viewer-host_15.55.3_amd64.deb
-./team-viewer-host_15.55.3_amd64.deb
+# sudo usermod -aG sudo (user)
+sudo apt install fastfetch firefox git xinit curl htop i3 thunar -y
+curl -fsSL https://download.opensuse.org/repositories/home:clayrisser:sid/Debian_Unstable/Release.key | gpg --dearmor | sudo tee /etc/apt/keyrings/home_clayrisser_sid.gpg > /dev/null
+ARCH="$(dpkg --print-architecture)"
+sudo tee /etc/apt/sources.list.d/home:clayrisser:sid.sources > /dev/null <<EOF
+Types: deb
+URIs: http://download.opensuse.org/repositories/home:/clayrisser:/sid/Debian_Unstable/
+Suites: /
+Architectures: $ARCH
+Signed-By: /etc/apt/keyrings/home_clayrisser_sid.gpg
+EOF
+sudo apt update
+sudo apt install ghostty-y
+echo "deb [arch=amd64,i386] https://repo.steampowered.com/steam/ stable steam" | sudo tee /etc/apt/sources.list.d/steam.list
+sudo apt update
+sudo apt install steam -y
