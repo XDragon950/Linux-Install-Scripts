e184b110972d1eaa3067b32e198300b57e8ab43f
XDragon950 <101992388+XDragon950@users.noreply.github.com>
2026-01-03 15:04:18 -0500

    Update debian.sh to install Steam and Chromium

--- debian.sh
+++ debian.sh
@@ -1,7 +1,9 @@
 # the first two are required if you install with a root password and another user account
 # apt install sudo
 # sudo usermod -aG sudo (user)
-sudo apt install fastfetch firefox xinit curl htop i3 thunar -y
+wget https://cdn.fastly.steamstatic.com/client/installer/steam.deb
+sudo dpkg -i steam.deb
+sudo apt install fastfetch chromium xinit curl htop i3 thunar -y
 curl -fsSL https://download.opensuse.org/repositories/home:clayrisser:sid/Debian_Unstable/Release.key | gpg --dearmor | sudo tee /etc/apt/keyrings/home_clayrisser_sid.gpg > /dev/null
 ARCH="$(dpkg --print-architecture)"
 sudo tee /etc/apt/sources.list.d/home:clayrisser:sid.sources > /dev/null <<EOF
@@ -13,6 +15,3 @@ Signed-By: /etc/apt/keyrings/home_clayrisser_sid.gpg
 EOF
 sudo apt update
 sudo apt install ghostty-y
-echo "deb [arch=amd64,i386] https://repo.steampowered.com/steam/ stable steam" | sudo tee /etc/apt/sources.list.d/steam.list
-sudo apt update
-sudo apt install steam -y
