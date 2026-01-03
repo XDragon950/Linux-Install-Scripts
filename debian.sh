fddad1612d7a1dbb72a1499c670689070a7ad9d1
XDragon950 <101992388+XDragon950@users.noreply.github.com>
2026-01-03 16:56:35 -0500

    Add curl to installation command in debian.sh

--- debian.sh
+++ debian.sh
@@ -1,7 +1,7 @@
 # the first two are required if you install with a root password and another user account
 # apt install sudo
 # sudo usermod -aG sudo (user)
-sudo apt install wget
+sudo apt install wget curl
 wget https://cdn.fastly.steamstatic.com/client/installer/steam.deb
 sudo dpkg -i steam.deb
 sudo apt install fastfetch chromium xinit curl htop i3 thunar zsh -y
