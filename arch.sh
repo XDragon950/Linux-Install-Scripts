ca0d7620186e65a3020f9e3fc043a765505f09b9
XDragon950 <101992388+XDragon950@users.noreply.github.com>
2026-01-29 07:49:50 -0500

    Update arch.sh

--- arch.sh
+++ arch.sh
@@ -1,1 +1,2 @@
-sudo pacman -S fastfetch zsh hyprland firefox 
+sudo pacman -S fastfetch zsh hyprland vivaldi curl
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
